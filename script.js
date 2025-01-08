0104bb0de76ee3c4189a24c749357398f902e7fa
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-08 13:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
-    //Write your code here
-  
+	let rad=document.getElementById("radius");
+	let vol=document.getElementById("volume");
+	let value=parseInt(rad.value);
+	if(isNaN(value) || value<0){
+		vol.innerHTML='NaN';
+	}else{
+		let calc=((4/3)*(22/7)*Math.pow(value,3)).toFixed(4);
+		vol.innerHTML=calc;
+	}
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = function(event) {
+        event.preventDefault(); // This line prevents the form from being submitted which would cause a page refresh.
+        volume_sphere();
+    };
+}
